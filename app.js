445973d810e1d7a78a5d35e67396f806eec8b9b7
Shreyash Nalawade <108988869+shreyashnalawade1@users.noreply.github.com>
2024-01-01 19:13:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors('*'));
 app.use(compression());
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tasks', taskRouter);
